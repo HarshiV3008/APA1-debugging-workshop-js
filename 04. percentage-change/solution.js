61f96730a3fcbd3bb6e0abeedf5922290e5622ac
HarshiV3008 <harshavivegad3@gmail.com>
2025-01-13 12:31:01 +0000

    04. PercentageChange

--- 04. percentage-change/solution.js
+++ 04. percentage-change/solution.js
@@ -1,8 +1,15 @@
 function calculatePercentageChange(original, newAmount) {
   const difference = Math.abs(newAmount - original);
-  const average = (original - newAmount) / 2;
-  const percentageDifference = (average / difference) * 100;
-  return percentageDifference.toFixed(2);
+  const percentageDifference = (difference/original) * 100;
+  if (difference === 0){
+    const change = 0;
+    return change;
+  }
+  if (newAmount < original) {
+    return - Math.round(percentageDifference);
+  }
+  return percentageDifference;
+  
 }
 
 module.exports = { calculatePercentageChange };
